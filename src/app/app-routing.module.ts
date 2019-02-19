14ba75f70857f60cb4ccc7e48d631e3a6ddcd00b
TranVietDuc <vietduc3k@gmail.com>
2019-02-19 08:13:39 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BlogComponent} from './blog/blog.component';
+import {BlogDetailComponent} from './blog-detail/blog-detail.component';
+import {BlogEditComponent} from './blog-edit/blog-edit.component';
+import {TodoComponent} from './todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'blog',
+  component: BlogComponent
+},
+  {
+    path: 'blog/:id',
+    component: BlogDetailComponent
+  },
+  {
+    path: 'blog/:id/edit',
+    component: BlogEditComponent
+  },
+  {
+    path: 'todo',
+    component: TodoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { TodoComponent } from './todo/todo.component';
+import {TokenInterceptor} from './token.interceptor';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    BlogEditComponent,
+    BlogDetailComponent,
+    TodoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
